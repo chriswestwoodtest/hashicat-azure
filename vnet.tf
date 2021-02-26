458d70e2f8e1dc94c501ee8957394421eb8f2b54
Chris Westwood <chris.westwoodgy@gmail.com>
2021-02-26 17:00:11 +0000

    Added vnet module - try 2

--- vnet.tf
+++ vnet.tf
@@ -2,5 +2,5 @@ module "network" {
   source  = "app.terraform.io/testcompany2021/network/azurerm"
   version = "3.0.1"
   # insert required variables here
-  resource_group_name = "azurerm_resource_group.myresourcegroup.name"
+  resource_group_name = "chrwestwood-workshop"
 }
